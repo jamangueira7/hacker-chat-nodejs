b4301cf1ee67cce025f2c16badcd3a6a4c274b30
João Mangueira <jamjs7@gmail.com>
2021-03-23 21:26:00 -0300

    add com as configs do client

--- client/index.js
+++ client/index.js
@@ -1,7 +1,11 @@
 import Events from 'events';
 import TerminalController from './src/terminalController.js';
+import CliConfig from './src/cliConfig.js';
+
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
 
 const componentEmitter = new Events();
 
-const controller = new TerminalController();
-await controller.initializeTable(componentEmitter);
+/*const controller = new TerminalController();
+await controller.initializeTable(componentEmitter);*/
