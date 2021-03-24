b4a0ba61ee00bcb57ddee2e701cd358e102589dd
João Mangueira <jamjs7@gmail.com>
2021-03-24 17:37:46 -0300

    os eventos estão atualizando a tela

--- client/index.js
+++ client/index.js
@@ -11,6 +11,9 @@ const componentEmitter = new Events();
 const socketClient = new SocketClient(config);
 await socketClient.initialize();
 const eventManager = new EventManager({ componentEmitter, socketClient });
+const events = eventManager.getEvents();
+socketClient.attachEvents(events);
+
 const data = {
     roomId: config.room,
     userName: config.username
